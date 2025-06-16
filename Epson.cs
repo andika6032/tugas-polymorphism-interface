b3500bf6cb0d7470b471737e314f06d82d6de520
andika6032 <andika32@students.amikom.ac.id>
2025-06-16 20:26:27 +0700

    Add files via upload

---
+++ Epson.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasPolymorphism
+{
+    class Epson : IPrinterWindows
+    {
+        public void Show()
+        {
+            Console.WriteLine("Printer Epson display dimension: 10*11");
+        }
+        public void Print()
+        {
+            Console.WriteLine("Printer Epson printing....");
+        }
+    }
+  
+ }
